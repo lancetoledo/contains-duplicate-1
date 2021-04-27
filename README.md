50a23aac6af819130896b8b634e81b9382e06005
lancetoledo <lanceadrian13@gmail.com>
2021-04-27 16:32:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# contains-duplicate-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/contains-duplicate-1)
---
+++ index.js
@@ -0,0 +1,25 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let nums = [1, 2, 3, 4];
+
+function findDupe(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        count += 1;
+        if (count >= 1) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+console.log(findDupe(nums));
